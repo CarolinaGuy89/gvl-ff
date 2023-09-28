109b5e8f2295dc31179ef96d2d4f31ba57a85969
Steven C <steventcruise@gmail.com>
2023-09-27 22:43:45 -0400

    Initial Team Overview

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,28 +1,92 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import Chart from 'chart.js/auto';
 
-export default function MyTeam({weeklyMatchup}) {
 
-  // useEffect(() => {
-  //   getBoxscoreForWeek(leagueId,selectedWeek).then((matchup) => {
-  //     // console.log('matchup: ', matchup);
-  //     setWeeklyMatchup(matchup);
-  //   })}, [leagueId, selectedWeek]);//Request new Data at change of League or week
+export default function MyTeam({ weeklyMatchup }) {
+  console.log('Team Entry Data', weeklyMatchup);
 
-console.log('Team ESPN Data', weeklyMatchup)
+  var combinedItems = []
+  weeklyMatchup.forEach((item) => {
+    // Create new objects for home and away items
+    const homeItem = {
+      score: item.homeScore,
+      benchScore: item.homeBenchScore,
+      result: item.homeResult,
+      roster: item.homeRoster,
+      manager: item.homeManager,
+      matchId: item.matchId,
+      weekId: item.weekId,
+    };
+
+    const awayItem = {
+      score: item.awayScore,
+      benchScore: item.awayBenchScore,
+      result: item.awayResult,
+      roster: item.awayRoster,
+      manager: item.awayManager,
+      matchId: item.matchId,
+      weekId: item.weekId,
+    };
+
+    // Push the objects into the combined array
+    combinedItems.push(homeItem);
+    combinedItems.push(awayItem);
+    combinedItems.sort((a, b) => b.score - a.score);
+  });
+
+
+  const chartRef = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
+  useEffect(() => {
+    console.log('chartRef.current',!chartRef.current)
+    if (combinedItems.length === 0) return;
+
+    // Destroy the previous chart instance, if it exists
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+    // Create the new chart
+
+    const deltaValues = combinedItems[2].roster.map((p) => p.delta)
+    console.log('deltaValues:' , deltaValues)
+    const labels = combinedItems[2].roster.map((item) => item.player.fullName);
+
+    const canvas = document.getElementById('teamPreformanceChart');
+    chartRef.current = new Chart(canvas, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Delta',
+            data: deltaValues,
+            backgroundColor: 'rgba(75, 192, 192, 1)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }, [weeklyMatchup]);
+
+
+  console.log('Team Exit Data', combinedItems)
   return (
     <>
-      <div>
-        <h2 style={{ textAlign: "center", color: "white", fontSize: "48px" }}>
-          This is Simon:
-          👷
-        </h2>
-        <h3 style={{ textAlign: "center", color: "white" }}>
-          Simon doesn't work very fast.
-
-        </h3>
-
-        <p style={{ textAlign: "center", color: "white" }}> <br />He will finish this page eventually...</p>
-      </div>
+          <h1 style={{ textAlign: "center", color: "white" }}>Actual Preformance relative to Projected</h1>
+          <h2 style={{ textAlign: "center", color: "white" }}>Test Data for {combinedItems[2].manager}/Week {combinedItems[2].weekId} </h2>
+          <div style={{background: "rgba(0, 0, 0, .6)"}}>
+            <canvas id="teamPreformanceChart" width="400" height="300"></canvas>
+          </div>
     </>
   )
 }
